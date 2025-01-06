9e040feb56ee6d141d30e614d988a2ed19a16c04
Tunuk <dfghn>
2025-01-06 21:13:29 +0600

    allow

--- src/main/java/org/example/moreman/config/RestConfig.java
+++ src/main/java/org/example/moreman/config/RestConfig.java
@@ -16,6 +16,7 @@ public class RestConfig {
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedOrigin("http://ec2-3-122-192-227.eu-central-1.compute.amazonaws.com");
+        config.addAllowedOrigin("http://ec2-3-122-192-227.eu-central-1.compute.amazonaws.com:8080");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
